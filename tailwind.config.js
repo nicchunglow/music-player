aa867ba0b0960f776994cfefe6f5056a1866d31e
nicchunglow <iamnicholaschung@gmail.com>
2024-02-03 19:45:05 +0800

    Update tailwind and configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      white: '#ffffff',
+      primaryAccent: '#716EFA',
+    },
   },
   plugins: [],
 }
-
