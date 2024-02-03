f3b16dbb8ed0bc3ebd2773e8e28dc131ddd35fd2
nicchunglow <iamnicholaschung@gmail.com>
2024-02-04 00:08:52 +0800

    Update App flow for state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Summary from './components/Summary'
 import SearchIcon from './assets/Search.svg'
 import DeleteIcon from './assets/Delete.svg'
 import { mockHistory } from './constants/mockData'
 
+type History = {
+  location: string
+  date: string
+}
+
 const App: React.FC = () => {
+  const [histories, setHistories] = useState<History[]>()
+
+  const getHistories = async () => {
+    setHistories(mockHistory)
+  }
+
+  useEffect(() => {
+    getHistories()
+  }, [])
+
   return (
     <>
       <div className='flex justify-center'>
@@ -16,22 +31,26 @@ const App: React.FC = () => {
                 <h2 aria-label='history-title' className=' flex py-5 text-sm'>
                   Search History
                 </h2>
-                {mockHistory.map((history, index) => (
-                  <div
-                    key={index}
-                    aria-label={`history-item-${index}`}
-                    className='my-4 flex h-16 items-start justify-around rounded-2xl bg-white bg-opacity-40 px-2 md:flex-row md:justify-between'
-                  >
-                    <span className='flex h-full w-full flex-col justify-center pr-5 md:w-full md:flex-row md:items-center md:justify-between'>
-                      <p className='text-sm'>{history.location}</p>
-                      <p className='text-sm'>{history.date}</p>
-                    </span>
-                    <span className='flex h-full w-1/5 items-center justify-end md:items-center'>
-                      <img src={SearchIcon} />
-                      <img src={DeleteIcon} />
-                    </span>
-                  </div>
-                ))}
+                {histories ? (
+                  histories.map((history, index) => (
+                    <div
+                      key={index}
+                      aria-label={`history-item-${index}`}
+                      className='my-4 flex h-16 items-start justify-around rounded-2xl bg-white bg-opacity-40 px-2 md:flex-row md:justify-between'
+                    >
+                      <span className='flex h-full w-full flex-col justify-center pr-5 md:w-full md:flex-row md:items-center md:justify-between'>
+                        <p className='text-sm'>{history.location}</p>
+                        <p className='text-sm'>{history.date}</p>
+                      </span>
+                      <span className='flex h-full w-1/5 items-center justify-end md:items-center'>
+                        <img src={SearchIcon} />
+                        <img src={DeleteIcon} />
+                      </span>
+                    </div>
+                  ))
+                ) : (
+                  <div></div>
+                )}
               </div>
             </div>
           </span>
