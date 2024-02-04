68fcfa54274ffc79df79178fcdbeae1263abff8e
nicchunglow <iamnicholaschung@gmail.com>
2024-02-04 10:53:19 +0800

    Update styling for weather img

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import Summary from './components/Summary'
 import { mockHistory } from './constants/mockData'
 import HistoryList from './components/HistoryList'
 import Sun from './assets/sun.png'
-import PurpleSearch from './assets/PurpleSearch.svg'
-import Button from './components/Button'
+import Search from './components/Search'
 
 type History = {
   location: string
@@ -25,21 +24,12 @@ const App: React.FC = () => {
   return (
     <>
       <div className='flex flex-col items-center justify-center'>
-        <span className=' flex h-40 w-11/12 items-start justify-between pt-5 md:h-48 md:w-10/12 lg:w-3/5 lg:max-w-[700px]'>
-          <input
-            type='text'
-            className='mr-5 flex h-[60px] w-11/12 rounded-3xl bg-white bg-opacity-20 px-4'
-            id='country-input'
-            name='country-input'
-            placeholder='Country'
-          />
-          <Button imgSrc={PurpleSearch} altText='country-search-button' />
-        </span>
+        <Search />
         <img
           width={'300px'}
           height={'300px'}
           src={Sun}
-          className='xm:w-56 absolute right-4 top-20 z-10 w-40 sm:right-28 md:right-32 md:w-72 lg:right-80'
+          className='xm:w-52 absolute right-4 top-20 z-10 w-40 sm:right-28 md:right-32 md:w-72 lg:right-80'
           alt='Sun'
         />
         <div className='z-0 flex h-full w-11/12 flex-col items-center rounded-3xl border-[1px] border-white border-opacity-50 bg-white bg-opacity-25 md:w-10/12 lg:w-3/5 lg:max-w-[700px]'>
