31ea87e29eac08746998f099f1fd5b2f6481afe7
Nicholas Chung <iamnicholaschung@gmail.com>
2024-02-15 15:08:39 +0800

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
-    base: process.env.NODE_ENV === 'production' ? '/nicchunglow/weather-app' : '/',
+    base: process.env.NODE_ENV === 'production' ? '/nicchunglow' : '/',
     define: {
       'process.env.BASE_URL': JSON.stringify(env.BASE_URL),
       'process.env.API_KEY': JSON.stringify(env.API_KEY),
