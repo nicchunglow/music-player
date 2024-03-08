049916619e715e6c888ab14c4ef034d679723ee4
nicchunglow <iamnicholaschung@gmail.com>
2024-03-09 00:57:05 +0800

    Update lint

--- src/components/SongQueueList/index.tsx
+++ src/components/SongQueueList/index.tsx
@@ -19,7 +19,7 @@ const SongQueueList: React.FC<SongQueueList> = ({ songs, songQueue }) => {
   return (
     <div
       className=' mx-4 flex h-[95vh] w-11/12
-         flex-col items-center justify-center rounded-3xl bg-white bg-opacity-25 md:w-10/12 lg:w-3/12 lg:max-w-[700px]'
+        flex-col items-center justify-center rounded-3xl bg-white bg-opacity-25 md:w-10/12 lg:w-3/12 lg:max-w-[700px]'
     >
       <h1 className='mt-4 font-bold'>Song Queue</h1>
       <div className='my-4 w-full overflow-y-scroll px-4'>
