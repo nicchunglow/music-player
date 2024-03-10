ec4080754613fd77ad80b5809a3c8d9f66c6f466
nicchunglow <iamnicholaschung@gmail.com>
2024-03-10 13:34:03 +0800

    Update music and songQueue ui

--- src/components/SongQueueList/index.tsx
+++ src/components/SongQueueList/index.tsx
@@ -19,12 +19,15 @@ const SongQueueList: React.FC<SongQueueList> = ({ songs, songQueue }) => {
   return (
     <div
       className=' mx-4 flex h-[95vh] w-11/12
-        flex-col items-center justify-center rounded-3xl bg-white bg-opacity-25 md:w-10/12 lg:w-3/12 lg:max-w-[700px]'
+        flex-col items-center rounded-3xl bg-white bg-opacity-25 md:w-10/12 lg:w-3/12 lg:max-w-[700px]'
     >
-      <h1 className='mt-4 font-bold'>Song Queue</h1>
-      <div className='my-4 w-full overflow-y-scroll px-4'>
+      <h1 className='mt-12 font-bold'>Song Queue</h1>
+      <span
+        className='my-4 flex h-full 
+        w-full flex-col items-center overflow-y-scroll px-4'
+      >
         {SongQueueGenerator}
-      </div>
+      </span>
     </div>
   )
 }
