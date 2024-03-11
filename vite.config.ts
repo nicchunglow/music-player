99116133f30c6d6b622529201992f8052032e151
nicchunglow <iamnicholaschung@gmail.com>
2024-03-11 21:43:07 +0800

    Update files

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
   return {
-    base: process.env.NODE_ENV === 'production' ? '/nicchunglow/weather-app' : '/',
-    define: {
-      'process.env.BASE_URL': JSON.stringify(env.BASE_URL),
-      'process.env.API_KEY': JSON.stringify(env.API_KEY),
-    },
     plugins: [react()],
     resolve: {
       alias: [{ find: '@', replacement: '/src' }],
