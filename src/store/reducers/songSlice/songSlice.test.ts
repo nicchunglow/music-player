064b794b6bc234f370f29bd155aa155061404be9
nicchunglow <iamnicholaschung@gmail.com>
2024-03-11 13:39:46 +0800

    Update songSlice test

--- src/store/reducers/songSlice/songSlice.test.ts
+++ src/store/reducers/songSlice/songSlice.test.ts
@@ -6,6 +6,7 @@ import songReducer, {
   togglePlaying,
   selectSong,
 } from '.'
+import * as songSliceHelper from './songSlice.helper'
 
 jest.mock('@/helper', () => ({
   songIdList: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
@@ -122,8 +123,28 @@ describe('songSlice', () => {
       expect(state.songQueue[0]).toEqual(initialState.currentSongId)
       expect(state.currentSongId).toEqual(initialState.currentSongId)
     })
+    it('should shuffle the songQueue and update currentSongId when isShuffled is true', () => {
+      const initialState: SongState = {
+        songQueue: [0, 1, 2, 3, 4],
+        previousSongQueue: [],
+        currentSongId: 0,
+        isPlaying: false,
+        isShuffled: false,
+      }
+
+      const currentSong = initialState.songQueue[0]
+      const shuffledList = [4, 1, 2, 0, 3]
+
+      jest.spyOn(songSliceHelper, 'shuffleList').mockReturnValue(shuffledList)
+
+      const newState = songReducer(initialState, toggleShuffle())
+
+      expect(newState.isShuffled).toBe(true)
+      expect(newState.songQueue).toEqual(shuffledList)
+      expect(newState.currentSongId).toEqual(currentSong)
+    })
     describe('Unshuffle', () => {
-      it('return a shuffled queue to the original queue, with the current song being the new starting point', () => {
+      it('should return a shuffled queue to the original queue, with the current song being the new starting point', () => {
         const initialState: SongState = {
           songQueue: [8, 2, 5],
           previousSongQueue: [],
@@ -136,7 +157,7 @@ describe('songSlice', () => {
         expect(state.isShuffled).not.toEqual(initialState.isShuffled)
         expect(state.songQueue).toEqual([8, 9])
       })
-      it('returns the original queue with the current song as the new starting point after selecting the next track and toggling shuffle', () => {
+      it('should return the original queue with the current song as the new starting point after selecting the next track', () => {
         const initialState: SongState = {
           songQueue: [8, 7, 5],
           previousSongQueue: [],
